599a4715b53e6486985129c61cdab36180cc8399
tjwogud <rlatjsal78@gmail.com>
2024-06-11 00:30:28 +0900

    fix

--- ImageCompress/Lz77Compress.cs
+++ ImageCompress/Lz77Compress.cs
@@ -2,29 +2,23 @@
 {
     public class Lz77Compress
     {
-        public struct LLD(int dist, int len, char c)
-        {
-            public int dist = dist, len = len;
-            public char c = c;
-        }
-
         public static byte[] Compress(byte[] data, ushort window = 32767, byte lookahead = 63)
         {
-            Span<int> input = new int[(int)Math.Ceiling((double)data.Length / 4)];
+            Span<ushort> input = new ushort[(ushort)Math.Ceiling((double)data.Length / 2)];
             BinaryReader reader = new(new MemoryStream(data));
             int index = 0;
-            while (reader.BaseStream.Length >= reader.BaseStream.Position + 4)
+            while (reader.BaseStream.Length >= reader.BaseStream.Position + 2)
             {
-                input[index] = reader.ReadInt32();
+                input[index] = reader.ReadUInt16();
                 index++;
             }
             if (reader.BaseStream.Length > reader.BaseStream.Position)
-                input[^1] = BitConverter.ToInt32([data[^1], 0, 0, 0]);
+                input[^1] = BitConverter.ToUInt16([data[^1], 0]);
 
             MemoryStream stream = new();
             BinaryWriter writer = new(stream);
 
-            void WriteLLD(int dist, int length, int c)
+            void WriteLLD(int dist, int length, ushort c)
             {
                 writer.Write((ushort)dist);
                 writer.Write(c);
@@ -36,8 +30,8 @@
             WriteLLD(0, 0, input[0]);
             for (int i = 1; i < input.Length; i++)
             {
-                ReadOnlySpan<int> windowBuff = input[Math.Max(0, i - window)..i];
-                ReadOnlySpan<int> lookaheadBuff = input[i..Math.Min(input.Length, i + lookahead)];
+                ReadOnlySpan<ushort> windowBuff = input[Math.Max(0, i - window)..i];
+                ReadOnlySpan<ushort> lookaheadBuff = input[i..Math.Min(input.Length, i + lookahead)];
                 var result = KmpSearch.SearchLongest(windowBuff, lookaheadBuff);
                 if (result.index == -1)
                     WriteLLD(0, 0, input[i]);
@@ -56,13 +50,13 @@
             BinaryReader reader = new(new MemoryStream(data));
 
             int size = reader.ReadInt32();
-            int[] chars = new int[size];
+            ushort[] chars = new ushort[size];
             int index = 0;
 
             while (reader.BaseStream.Position < reader.BaseStream.Length)
             {
                 ushort dist = reader.ReadUInt16();
-                int c = reader.ReadInt32();
+                ushort c = reader.ReadUInt16();
                 if (dist != 0)
                 {
                     byte length = reader.ReadByte();
@@ -75,7 +69,7 @@
 
             MemoryStream stream = new();
             BinaryWriter writer = new(stream);
-            foreach (int c in chars)
+            foreach (ushort c in chars)
                 writer.Write(c);
 
             return stream.ToArray();
